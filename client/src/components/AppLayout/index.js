671c4fb7df4657372418bcdc84bf9d95f82b65f2
Phong Luong <luonghongphong6703@gmail.com>
2023-05-20 22:15:48 +0700

    phongluong update meta title

--- client/src/components/AppLayout/index.js
+++ client/src/components/AppLayout/index.js
@@ -1,3 +1,3 @@
-export { default as HeaderOnly} from './HeaderOnly'
+export { default as HeaderOnly } from './HeaderOnly'
 
-export { default as DefaultLayout} from './DefaultLayout'
+export { default as DefaultLayout } from './DefaultLayout'
