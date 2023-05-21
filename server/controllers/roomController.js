5ed23d7fb2bc9efaa4e1c7539d6e220656dcd0cc
ducnm71 <21021293@vnu.edu.vn>
2023-05-21 18:47:17 +0700

    Sua loi 2 user tao 2 room trung ten nhau

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -2,19 +2,32 @@ const asyncHandler = require('express-async-handler')
 const zoomModel = require('../models/roomModel')
 const userModel = require('../models/userModel')
 
+
+
 const createZoom = asyncHandler(async(req, res) => {
     const {name} = req.body
     const userId = req.params.id
-    const checkExist = await zoomModel.findOne({name})
-    if (checkExist) {
+    const checkUser = await userModel.findById(userId).populate('rooms')
+    let checkZoom
+    for (let i = 0; i < checkUser.rooms.length; i++){
+        if (checkUser.rooms[i] === name){
+            checkZoom = true
+            break
+        }else{
+            checkZoom = false
+        }
+        
+    }
+    if (checkZoom) {
         res.status(401)
         throw new Error('This zoom has already existed!')
     }
 
     const newZoom = await zoomModel.create({name})
     if (newZoom){
+        checkUser.rooms.push(newZoom._id)
+        await checkUser.save()
         res.status(200).json(newZoom)
-        await userModel.updateOne({_id: userId}, { $push: { zooms: newZoom._id}})
     }else {
         res.status(400)
         throw new Error('Invalid data of zoom!')
