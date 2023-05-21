b3cd6952a801df7fe8fd9ce3a50b19d74097e3a8
ducnm71 <21021293@vnu.edu.vn>
2023-05-21 20:24:13 +0700

    Sua loi 2 user tao 2 room trung ten nhau

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -2,36 +2,38 @@ const asyncHandler = require('express-async-handler')
 const zoomModel = require('../models/roomModel')
 const userModel = require('../models/userModel')
 
-
+const checkZoom = (arr, input) => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].name === input){
+            return true
+        }
+    }
+    return false
+}
 
 const createZoom = asyncHandler(async(req, res) => {
     const {name} = req.body
     const userId = req.params.id
     const checkUser = await userModel.findById(userId).populate('rooms')
-    let checkZoom
-    for (let i = 0; i < checkUser.rooms.length; i++){
-        if (checkUser.rooms[i] === name){
-            checkZoom = true
-            break
-        }else{
-            checkZoom = false
-        }
-        
-    }
-    if (checkZoom) {
+    console.log(checkUser);
+    const check = checkZoom(checkUser.rooms, name)
+    
+    if (check) {
         res.status(401)
         throw new Error('This zoom has already existed!')
+    } else {
+
+        const newZoom = await zoomModel.create({name})
+        if (newZoom){
+            checkUser.rooms.push(newZoom._id)
+            await checkUser.save()
+            res.status(200).json(newZoom)
+        }else {
+            res.status(400)
+            throw new Error('Invalid data of zoom!')
+        }
     }
 
-    const newZoom = await zoomModel.create({name})
-    if (newZoom){
-        checkUser.rooms.push(newZoom._id)
-        await checkUser.save()
-        res.status(200).json(newZoom)
-    }else {
-        res.status(400)
-        throw new Error('Invalid data of zoom!')
-    }
 })
 
 const getPin = asyncHandler(async(req,res) => {
@@ -43,7 +45,7 @@ const getPin = asyncHandler(async(req,res) => {
     }
 
     checkZoom.pin = Math.random().toString(36).substring(2, 8)
-    await res.status(200).json(checkZoom)
+    res.status(200).json(checkZoom)
 
 })
 
