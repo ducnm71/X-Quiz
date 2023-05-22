be68b6eccb1c4a3be44cd7bcfee606cb4a7d4015
tttd16 <tiendung.work16@gmail.com>
2023-05-23 03:14:22 +0700

    fix bug user no confirm password

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,27 +1,30 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const registerValidate = (req, res, next) => {
-    const data = req.body
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(8).required()
-    })
-    const { error } = schema.validate(data)
-    if (error) return res.status(400).send(error.details[0].message)
-    next()
-
-}
+  const data = req.body;
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+    confirmPassword: Joi.string()
+      .valid(Joi.ref('password'))
+      .required()
+      .messages('Confirm password must match the password'),
+  });
+  const { error } = schema.validate(data);
+  if (error) return res.status(400).send(error.details[0].message);
+  next();
+};
 
 const loginValidate = (req, res, next) => {
-    const data = req.body
-    const schema = Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().min(8).required()
-    })
-    const { error } = schema.validate(data)
-    if (error) return res.status(400).send(error.details[0].message)
-    next()
-}
+  const data = req.body;
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+  const { error } = schema.validate(data);
+  if (error) return res.status(400).send(error.details[0].message);
+  next();
+};
 
-module.exports = { registerValidate, loginValidate }
+module.exports = { registerValidate, loginValidate };
