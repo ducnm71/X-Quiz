376d565616a3afffc7af34876c61b07febdb1293
Phong Luong <luonghongphong6703@gmail.com>
2023-06-06 20:10:55 +0700

    fix by antd

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,11 +1,18 @@
-import { Carousel, Button, Input } from 'antd';
+import {
+  Carousel,
+  Button,
+  Input,
+  Form,
+  Typography,
+  Col,
+  Row,
+} from 'antd';
 
 import './index.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-  const [open, setOpen] = useState(false);
 
   return (
     <React.Fragment>
@@ -14,9 +21,21 @@ const HomePage = () => {
         <div className="homepage--slide-2 mySlide"></div>
         <div className="homepage--slide-3 mySlide"></div>
       </Carousel>
-      <div className='home-form-container'>
-        <div className='home--description'>
-          <h1 className='home--desc__title'>
+      <Row
+        className='home-form-container'
+      >
+        <Col
+          span={12}
+          className='home--description'
+        >
+          <Typography.Title
+            style={{
+              color: "white",
+              fontSize: 50,
+              wordSpacing: -3,
+            }}
+            className='home--desc__title'
+          >
             <span
               style={{
                 color: "#f86e2f"
@@ -24,64 +43,89 @@ const HomePage = () => {
               X-Quizz
             </span> {""}
             is a smart choice for school, friends and family.
-          </h1>
-          <p className='home--desc__subtitle'>
+          </Typography.Title>
+          <Typography.Text
+            className='home--desc__subtitle'
+          >
             What is X-Quizz!? X-Quizz! is a game-based learning platform that makes it easy to create,
             share and play learning games or trivia quizzes in minutes.
             Unleash the fun in classrooms, offices and living rooms!
-          </p>
-          <Button
-            type={"primary"}
-            className='home--desc__btn'
-          >
-            Create Room
-          </Button>
-          <Button
-            type={"transparent"}
-            style={{
-              borderColor: "white",
-              borderWidth: 2
-            }}
-            className='home--desc__btn'
-          >
-            Play
-          </Button>
-        </div>
+          </Typography.Text>
+          <br />
+          <div>
+            <Button
+              type={"primary"}
+              className='home--desc__btn'
+              size={'large'}
+            >
+              Create Room
+            </Button>
+            <Button
+              type={"transparent"}
+              size={'large'}
+              style={{
+                borderColor: "white",
+                borderWidth: 2
+              }}
+              className='home--desc__btn'
+            >
+              Play
+            </Button>
+          </div>
+        </Col>
 
-        <div className='home--signUpForm-container'>
-          <div className='home--signUpForm'>
-            <h1 className='home--signUpForm-title'>
+        <Col
+          span={12}
+          className='home--signUpForm-container'
+        >
+          <Form
+            className='home--signUpForm'
+            size={'large'}
+            layout="vertical"
+            style={{
+              height: 'fit-content'
+            }}
+          >
+            <Typography.Title
+              className='home--signUpForm-title'
+            >
               Sign Up to create your free Game
-            </h1>
-            <div className='home--signUpForm__warningText'>
-              All fields are required*
-            </div>
-            <Input
-              placeHolder={'First name'}
-              className='home--signUpForm__input'
-            />
-            <Input
-              placeHolder={'Last name'}
-              className='home--signUpForm__input'
-            />
-            <Input
-              placeHolder={'Email address'}
-              className='home--signUpForm__input'
-            />
-            <Input
-              placeHolder={'Phone number'}
-              className='home--signUpForm__input'
-            />
+            </Typography.Title>
+            <Form.Item>
+              <Input
+                placeHolder={'First name'}
+                className='home--signUpForm__input'
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                placeHolder={'Last name'}
+                className='home--signUpForm__input'
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                placeHolder={'Email address'}
+                className='home--signUpForm__input'
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                placeHolder={'Phone number'}
+                className='home--signUpForm__input'
+              />
+            </Form.Item>
             <Button className='home--signUpForm__btn'>
               Sign Up Now
             </Button>
-            <div className='home--signUpForm__privacy'>
+            <br />
+            <Typography.Text className='home--signUpForm__privacy'>
               Your information will not be shared with anyone {" "}
               <Link>(See Privacy policy)</Link>
-            </div>
-          </div>
-        </div>
-      </div>
+            </Typography.Text>
+          </Form>
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
