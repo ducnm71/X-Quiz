70933b536d9fa6f246ad3883b15557aee3a2accb
Phong Luong <luonghongphong6703@gmail.com>
2023-06-07 09:58:55 +0700

    fix homepage by antd

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -23,7 +23,6 @@ const HomePage = () => {
         <div className="homepage--slide-2 mySlide"></div>
         <div className="homepage--slide-3 mySlide"></div>
       </Carousel>
-<<<<<<< phongluong
       <Row
         className='home-form-container'
       >
@@ -39,21 +38,13 @@ const HomePage = () => {
             }}
             className='home--desc__title'
           >
-=======
-      <div className="home-form-container">
-        <div className="home--description">
-          <h1 className="home--desc__title">
->>>>>>> main
             <span
               style={{
-                color: '#f86e2f',
-              }}
-            >
+                color: "#f86e2f"
+              }}>
               X-Quizz
-            </span>{' '}
-            {''}
+            </span> {""}
             is a smart choice for school, friends and family.
-<<<<<<< phongluong
           </Typography.Title>
           <Typography.Text
             className='home--desc__subtitle'
@@ -137,43 +128,6 @@ const HomePage = () => {
           </Form>
         </Col>
       </Row>
-=======
-          </h1>
-          <p className="home--desc__subtitle">
-            What is X-Quizz!? X-Quizz! is a game-based learning platform that makes it easy to create, share and play
-            learning games or trivia quizzes in minutes. Unleash the fun in classrooms, offices and living rooms!
-          </p>
-          <Button type={'primary'} className="home--desc__btn">
-            Create Room
-          </Button>
-          <Button
-            type={'transparent'}
-            style={{
-              borderColor: 'white',
-              borderWidth: 2,
-            }}
-            className="home--desc__btn"
-          >
-            Play
-          </Button>
-        </div>
-
-        <div className="home--signUpForm-container">
-          <div className="home--signUpForm">
-            <h1 className="home--signUpForm-title">Sign Up to create your free Game</h1>
-            <div className="home--signUpForm__warningText">All fields are required*</div>
-            <Input placeHolder={'Name'} className="home--signUpForm__input" />
-            <Input placeHolder={'Email'} className="home--signUpForm__input" />
-            <Input placeHolder={'Password'} className="home--signUpForm__input" />
-            <Input placeHolder={'Confirm Password'} className="home--signUpForm__input" />
-            <Button className="home--signUpForm__btn">Sign Up Now</Button>
-            <div className="home--signUpForm__privacy">
-              Your information will not be shared with anyone <Link>(See Privacy policy)</Link>
-            </div>
-          </div>
-        </div>
-      </div>
->>>>>>> main
     </React.Fragment>
   );
 };
