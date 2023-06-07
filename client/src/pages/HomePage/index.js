1161a77b7317fedc86bf37ca1349a201a28c86b2
tttd16 <tiendung.work16@gmail.com>
2023-06-07 17:24:31 +0700

    Save changes before merging

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,20 +1,12 @@
-import {
-  Carousel,
-  Button,
-  Input,
-  Form,
-  Typography,
-  Col,
-  Row,
-} from 'antd';
+import { Carousel, Button, Input } from 'antd';
 
 import './index.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import withAuth from '~/redux/withAuth';
 
 const HomePage = () => {
-
+  // const [open, setOpen] = useState(false);
 
   return (
     <React.Fragment>
@@ -23,27 +15,9 @@ const HomePage = () => {
         <div className="homepage--slide-2 mySlide"></div>
         <div className="homepage--slide-3 mySlide"></div>
       </Carousel>
-<<<<<<< phongluong
-      <Row
-        className='home-form-container'
-      >
-        <Col
-          span={12}
-          className='home--description'
-        >
-          <Typography.Title
-            style={{
-              color: "white",
-              fontSize: 50,
-              wordSpacing: -3,
-            }}
-            className='home--desc__title'
-          >
-=======
       <div className="home-form-container">
         <div className="home--description">
           <h1 className="home--desc__title">
->>>>>>> main
             <span
               style={{
                 color: '#f86e2f',
@@ -53,91 +27,6 @@ const HomePage = () => {
             </span>{' '}
             {''}
             is a smart choice for school, friends and family.
-<<<<<<< phongluong
-          </Typography.Title>
-          <Typography.Text
-            className='home--desc__subtitle'
-          >
-            What is X-Quizz!? X-Quizz! is a game-based learning platform that makes it easy to create,
-            share and play learning games or trivia quizzes in minutes.
-            Unleash the fun in classrooms, offices and living rooms!
-          </Typography.Text>
-          <br />
-          <div>
-            <Button
-              type={"primary"}
-              className='home--desc__btn'
-              size={'large'}
-            >
-              Create Room
-            </Button>
-            <Button
-              type={"transparent"}
-              size={'large'}
-              style={{
-                borderColor: "white",
-                borderWidth: 2
-              }}
-              className='home--desc__btn'
-            >
-              Play
-            </Button>
-          </div>
-        </Col>
-
-        <Col
-          span={12}
-          className='home--signUpForm-container'
-        >
-          <Form
-            className='home--signUpForm'
-            size={'large'}
-            layout="vertical"
-            style={{
-              height: 'fit-content'
-            }}
-          >
-            <Typography.Title
-              className='home--signUpForm-title'
-            >
-              Sign Up to create your free Game
-            </Typography.Title>
-            <Form.Item>
-              <Input
-                placeHolder={'First name'}
-                className='home--signUpForm__input'
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                placeHolder={'Last name'}
-                className='home--signUpForm__input'
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                placeHolder={'Email address'}
-                className='home--signUpForm__input'
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                placeHolder={'Phone number'}
-                className='home--signUpForm__input'
-              />
-            </Form.Item>
-            <Button className='home--signUpForm__btn'>
-              Sign Up Now
-            </Button>
-            <br />
-            <Typography.Text className='home--signUpForm__privacy'>
-              Your information will not be shared with anyone {" "}
-              <Link>(See Privacy policy)</Link>
-            </Typography.Text>
-          </Form>
-        </Col>
-      </Row>
-=======
           </h1>
           <p className="home--desc__subtitle">
             What is X-Quizz!? X-Quizz! is a game-based learning platform that makes it easy to create, share and play
@@ -173,7 +62,6 @@ const HomePage = () => {
           </div>
         </div>
       </div>
->>>>>>> main
     </React.Fragment>
   );
 };
