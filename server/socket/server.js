fa0ea63ff3333100c5f6b1506185e83af25b7538
ducnm71 <21021293@vnu.edu.vn>
2023-06-23 02:57:05 +0700

    update socket join room

--- server/socket/server.js
+++ server/socket/server.js
@@ -13,23 +13,28 @@ const socketApi = () => {
     socket.on('join', async({pin, name}) => {
       try{
           
-          const checkRoom = await roomModel.findOne({pin: pin})
+          const checkRoom = await roomModel.findOne({pin: pin}).populate('players')
           
           if (!checkRoom) {
             socket.emit('roomNotFound')
           } else {
+                const checkExist = checkRoom.players.find(players => players.name === name)
+                if (checkExist){
+                    socket.emit('existed')
+                } else{
 
-              socket.join(pin)
-              const newPlayer = await playerModel.create({ name});
-              if (newPlayer) {
-                  checkRoom.players.push(newPlayer._id);
-                  newPlayer.roomId = checkRoom._id;
-                  await checkRoom.save();
-                  await newPlayer.save();
-                
-                  const players = await playerModel.find({roomId: checkRoom._id})
-                  io.to(pin).emit('updatePlayers', players)
-              }
+                    socket.join(pin)
+                    const newPlayer = await playerModel.create({ name});
+                    if (newPlayer) {
+                        checkRoom.players.push(newPlayer._id);
+                        newPlayer.roomId = checkRoom._id;
+                        await checkRoom.save();
+                        await newPlayer.save();
+                      
+                        const players = await playerModel.find({roomId: checkRoom._id})
+                        io.to(pin).emit('updatePlayers', players)
+                    }
+                }
             }
         } catch (err) {
             console.error(err)
@@ -38,31 +43,25 @@ const socketApi = () => {
     
     let intervalId;
     let questionIndex = 0;
+    let roomPin = '';
                                             
-    // const sendQuestion = (questions) =>{
-    //     if (questionIndex < questions.length) {
-    //         const question = questions[questionIndex];
-    //         socket.emit('question', question);
-    //         questionIndex++;
-    //     } else {
-    //         clearInterval(intervalId);
-    //     }
-    // }
-                                            
-    socket.on('startQuiz',async (pin) => {
-        const room = await roomModel.findOne({pin: pin}).populate('questions')
+    const sendQuestion = async () =>{
+        const room = await roomModel.findOne({pin: roomPin}).populate('questions')
         const questions = room.questions
+        if (questionIndex < questions.length) {
+            const question = questions[questionIndex];
+            socket.emit('question', question);
+            questionIndex++;
+        } else {
+            clearInterval(intervalId);
+        }
+    }
+                                            
+    socket.on('startQuiz', (pin) => {
+        roomPin = pin;
         questionIndex = 0;
-        // sendQuestion(questions);
-        intervalId = setInterval(() => {
-            if (questionIndex < questions.length) {
-                const question = questions[questionIndex];
-                socket.emit('question', question);
-                questionIndex++;
-            } else {
-                clearInterval(intervalId);
-            }
-        }, 1000);
+        sendQuestion();
+        intervalId = setInterval(sendQuestion, 3000)
     });
                                             
     socket.on('stopQuiz', () => {
@@ -73,6 +72,7 @@ const socketApi = () => {
     // Xử lý sự kiện khi một người chơi ngắt kết nối
     socket.on('disconnect', function() {
       console.log('A client disconnected');
+      clearInterval(intervalId)
     });
   });
 }
