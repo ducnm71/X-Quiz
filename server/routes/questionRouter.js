ad96ffa5716f0642e1eb793ac5ca82331d939219
ducnm71 <21021293@vnu.edu.vn>
2023-06-26 00:41:07 +0700

    update question

--- server/routes/questionRouter.js
+++ server/routes/questionRouter.js
@@ -5,6 +5,6 @@ const { addQuestion, getQuestions } = require('../controllers/questionController
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/addquestion/:id',addQuestion);
-router.get('/getques/:pin', getQuestions);
+router.get('/getques/:id', getQuestions);
 
 module.exports = router;
