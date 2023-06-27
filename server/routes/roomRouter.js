fe2bacff515e8a23f3d96867879c6d0ae6ad7b61
tttd16 <tiendung.work16@gmail.com>
2023-06-27 10:38:09 +0700

    update server

--- server/routes/roomRouter.js
+++ server/routes/roomRouter.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const {createRoom, getPin,getRoom} = require ('../controllers/roomController.js')
-const {protect} = require('../middleware/authMiddleware.js')
+const { createRoom, getPin, getRoom, deleteRoom } = require('../controllers/roomController.js');
+const { protect } = require('../middleware/authMiddleware.js');
 
-router.post('/createroom/:id',createRoom)
-router.get('/getpin/:id', getPin)
-router.get('/getroom/:userid', getRoom)
+router.get('/getpin/:id', getPin);
+router.get('/getroom/:userid', getRoom);
+router.post('/createroom/:id', createRoom);
+router.delete('/:idroom', deleteRoom);
 
-module.exports = router
+module.exports = router;
