8351e4d2967bd892e00cd5803c5ad4ec319ce04d
ducnm71 <21021293@vnu.edu.vn>
2023-06-27 20:42:18 +0700

    delete question

--- server/routes/questionRouter.js
+++ server/routes/questionRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { addQuestion, getQuestions } = require('../controllers/questionController');
+const { addQuestion, getQuestions, deleteQuestion } = require('../controllers/questionController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/addquestion/:id',addQuestion);
 router.get('/getques/:id', getQuestions);
+router.delete('/delques/:quesId/:roomId', deleteQuestion)
 
 module.exports = router;
