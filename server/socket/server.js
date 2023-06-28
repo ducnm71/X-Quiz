55eda75a357fbf9f34c255a48b6edafa2ece7989
tttd16 <tiendung.work16@gmail.com>
2023-06-28 17:03:36 +0700

    update

--- server/socket/server.js
+++ server/socket/server.js
@@ -46,7 +46,7 @@ const socketApi = () => {
           if (checkExist) {
             socket.emit('existed');
           } else {
-            socket.emit('joined', { pin, name });
+            //socket.emit('joined', { pin, name });
             socket.join(pin);
             const newPlayer = await playerModel.create({ name });
             if (newPlayer) {
@@ -58,50 +58,34 @@ const socketApi = () => {
               const players = await playerModel.find({ roomId: checkRoom._id });
               io.to(pin).emit('updatePlayers', players);
 
-                    socket.join(pin)
-                    const newPlayer = await playerModel.create({ name});
-                    if (newPlayer) {
-                        checkRoom.players.push(newPlayer._id);
-                        newPlayer.roomId = checkRoom._id;
-                        await checkRoom.save();
-                        await newPlayer.save();
-
-                        const players = await playerModel.find({roomId: checkRoom._id})
-                        io.to(pin).emit('updatePlayers', players)
-                        
-                        socket.on('leave', async () => {
-                          socket.leave(pin)
-                          const player = await playerModel.findByIdAndDelete(newPlayer._id)
-                          console.log(player.name + ' lelf');
-                          checkRoom.players = checkRoom.players.filter((player) => player._id !== newPlayer._id)
-                          await checkRoom.save()
-                          const players = await playerModel.find({roomId: checkRoom._id})
-                          io.to(pin).emit('updatePlayers', players)
-                          
-                        })
-                        socket.on('answer',async ({selectedAnswerIndex, qi}) => {
-                          const room = await roomModel.findOne({pin: pin}).populate('questions')
-                          const questions = room.questions
-                          if (qi >= 0 && qi <= questions.length) {
-                              const currentQuestion = questions[qi];
-                              const correctAnswer = currentQuestion.correctAnswer
-                              if (selectedAnswerIndex === currentQuestion.options[correctAnswer]) {
-                                newPlayer.score += 10;
-                                io.to(pin).emit('answerResult', { isCorrect: true, score: newPlayer.score });
-                              } else {
-                                io.to(pin).emit('answerResult', { isCorrect: false, score: newPlayer.score });
-                              }
-                            }
-                    
-                            await newPlayer.save()
-                          });
-                    }
-
+              socket.on('leave', async () => {
+                socket.leave(pin);
+                const player = await playerModel.findByIdAndDelete(newPlayer._id);
+                console.log(player.name + ' lelf');
+                checkRoom.players = checkRoom.players.filter((player) => player._id !== newPlayer._id);
+                await checkRoom.save();
+                const players = await playerModel.find({ roomId: checkRoom._id });
+                io.to(pin).emit('updatePlayers', players);
+              });
+              socket.on('answer', async ({ selectedAnswerIndex, qi }) => {
+                const room = await roomModel.findOne({ pin: pin }).populate('questions');
+                const questions = room.questions;
+                if (qi >= 0 && qi <= questions.length) {
+                  const currentQuestion = questions[qi];
+                  const correctAnswer = currentQuestion.correctAnswer;
+                  if (selectedAnswerIndex === currentQuestion.options[correctAnswer]) {
+                    newPlayer.score += 10;
+                    io.to(pin).emit('answerResult', { isCorrect: true, score: newPlayer.score });
+                  } else {
+                    io.to(pin).emit('answerResult', { isCorrect: false, score: newPlayer.score });
+                  }
                 }
 
                 await newPlayer.save();
               });
             }
+
+            await newPlayer.save();
           }
         }
       } catch (err) {
@@ -125,9 +109,8 @@ const socketApi = () => {
     // Xử lý sự kiện khi một người chơi ngắt kết nối
     socket.on('clientDisconnect', function () {
       console.log('A client disconnected');
-      socket.leave(roomPin)
-      clearInterval(intervalId)
-
+      socket.leave(roomPin);
+      clearInterval(intervalId);
     });
   });
 };
